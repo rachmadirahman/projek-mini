7abe4f8dd92ee1bc25ef1ee2986afba6cbfa0003
rachmadirahman <72085680+rachmadirahman@users.noreply.github.com>
2022-07-03 00:57:28 +0700

    first commit

---
+++ cpanel/home.php
@@ -0,0 +1,10 @@
+<div align="center">
+<h2>
+Aplikasi KNN dalam  Kelulusan Hasil Bimbingan Mahad Bimbingan Islam
+</h2>
+
+<?php
+$R=rand(1,7);
+?>
+<img src="ypathfile/mahad<?php echo $R;?>.jpg" width="100%" height="350" title="<?php echo $tittle;?>">
+</div>
